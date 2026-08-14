b55b3cc4de66ce64a22d23bee48a57558aa95c82
KRUPAL MEWADA <krupalmewada17@gmail.com>
2026-08-14 02:05:38 -0400

    003_updated_at_trigger file added

--- migrations/003_updated_at_trigger.sql
+++ migrations/003_updated_at_trigger.sql
@@ -4,3 +4,36 @@
 -- One shared plpgsql function, wired to every table that has an updated_at column:
 -- users, user_data, workouts.
 -- Author: Krupal | Created: 2026-08-13
+
+BEGIN;
+
+DROP FUNCTION IF EXISTS set_updated_at() CASCADE;
+
+CREATE FUNCTION set_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+
+NEW.updated_at := NOW();
+    RETURN NEW; -- Returns the modified row to be saved
+
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_users_updated_at
+BEFORE UPDATE
+ON users
+FOR EACH ROW EXECUTE FUNCTION set_updated_at();
+
+CREATE TRIGGER trg_user_data_updated_at
+BEFORE UPDATE
+ON user_data
+FOR EACH ROW EXECUTE FUNCTION set_updated_at();
+
+CREATE TRIGGER trg_workouts_updated_at
+BEFORE UPDATE
+ON workouts
+FOR EACH ROW EXECUTE FUNCTION set_updated_at();
+
+
+
+COMMIT;
